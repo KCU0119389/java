2b4403f9551d9fa77f08225a8b114f3919e4ebd6
satwik1111 <naikn5637@gmail.com>
2025-04-09 10:28:46 +0530

    hello

---
+++ First.java
@@ -0,0 +1,15 @@
+public class First {
+
+        public static void main(String[] args)
+        {
+            char a = 'G';
+            int i =  34;
+            byte b = 8;
+    
+            System.out.println("char:"+a);
+            System.out.println("integer:"+i);
+            System.out.println("byte:"+b);
+    
+        }
+    }
+
