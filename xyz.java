281f6d17fcd8b2170c22ae4fd54673a16f5d39f6
arundati-huragi <arundatikhuragi@gmail.com>
2025-04-23 10:36:38 +0530

    hello

---
+++ xyz.java
@@ -0,0 +1,19 @@
+public class xyz {
+    public static void main(String[] args) {
+        String fruit = "Apple";
+
+        switch (fruit) {
+            case "Apple":
+                System.out.println("You chose an Apple!");
+                break;
+            case "Banana":
+                System.out.println("You chose a Banana!");
+                break;
+            case "Orange":
+                System.out.println("You chose an Orange!");
+                break;
+            default:
+                System.out.println("Unknown fruit.");
+        }
+    }
+}
